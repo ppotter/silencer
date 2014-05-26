ac33d85ce22ef201f1bedb32c53b5baa00b6b0fa
Patrick Potter <patrick.potter2@gmail.com>
2014-05-26 12:11:16 -0400

    Adding in app license.

---
+++ src/com/potter/silencer/ui/LicenseActivity.java
@@ -0,0 +1,18 @@
+package com.potter.silencer.ui;
+
+import com.potter.silencer.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LicenseActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.license_page);
+	}
+	
+	
+
+}
